286fbfaba737f3f17eb38593fa82ae11d922d473
Vivek <>
2024-09-01 00:15:39 +0530

    Added Basic UI(NavGraph is Pending)

--- app/src/main/java/com/vivek/movietrend/MainActivity.kt
+++ app/src/main/java/com/vivek/movietrend/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.vivek.movietrend
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.vivek.movietrend.network.ApiResult
 import com.vivek.movietrend.presentation.MovieViewModel
 import com.vivek.movietrend.ui.theme.MovieTrendTheme
@@ -31,8 +47,7 @@ class MainActivity : ComponentActivity() {
             MovieTrendTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Android", modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -42,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val  viewModel: MovieViewModel = hiltViewModel()
+    val viewModel: MovieViewModel = hiltViewModel()
     val moviesState by viewModel.trendingMovies.collectAsState()
     LaunchedEffect(Unit) {
         viewModel.fetchTrendingMovies()
@@ -50,19 +65,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     when (val result = moviesState) {
         is ApiResult.Loading -> {
-            CircularProgressIndicator()
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
         }
+
         is ApiResult.Success -> {
-            Log.e("Vivek",result.data.toString())
+            LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 150.dp)) {
+                items(result.data ?: emptyList()) { movie ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        AsyncImage(
+                            model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+                            contentDescription = movie.title,
+                            modifier = Modifier.size(250.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            maxLines = 2,
+                            text = movie.title,
+                            style = MaterialTheme.typography.titleSmall,
+                        )
+                    }
+                }
+            }
         }
+
         is ApiResult.Error -> {
             Text(text = "Error: ${result.exception.message}")
         }
     }
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/vivek/movietrend/data/remote/MoviesService.kt
+++ app/src/main/java/com/vivek/movietrend/data/remote/MoviesService.kt
@@ -1,17 +1,21 @@
 package com.vivek.movietrend.data.remote
 
+import com.vivek.movietrend.BuildConfig
 import com.vivek.movietrend.data.model.MovieDetailResponse
 import com.vivek.movietrend.data.model.MovieResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface MoviesService {
     @GET("movie/popular")
-    suspend fun getTrendingMovies(): MovieResponse
+    suspend fun getTrendingMovies(
+        @Header("Authorization") token : String = "Bearer "+BuildConfig.TOKEN
+    ): MovieResponse
 
     @GET("movie/{id}")
     suspend fun getMovieDetails(
-        @Path("id") id: String
+        @Path("id") id: String,
+        @Header("Authorization") token : String = "Bearer "+BuildConfig.TOKEN
     ): MovieDetailResponse
 }
